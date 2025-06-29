f20e4aede8411957b3c827dead66736ead04bcf1
gasanamous <162181627+gasanamous@users.noreply.github.com>
2025-06-29 20:16:56 +0300

    Update socket.js

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client'
 
 const token = localStorage.getItem('adminToken')
+
+console.log('API:', import.meta.env.VITE_API)
+console.log('SOCKET:', import.meta.env.VITE_API_SOCKET)
+
 const socket = io(import.meta.env.VITE_API_SOCKET, {
     extraHeaders: {
         token
